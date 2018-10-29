d77fd38505db0801eb53bc1709f55bbdb55d9f09
StarZoOm <kulevkir@gmail.com>
2018-10-29 21:18:35 +0300

    first commit

--- mpl_sqares.py
+++ mpl_sqares.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 
-input_values = [1, 2, 3, 4, 5]
-squares = [1, 4, 9, 16, 25]
+input_values = [1, 2, 3, 4, 5, 6]
+squares = [1, 4, 9, 16, 25, 36]
 plt.plot(input_values, squares, linewidth = 5)
 plt.title('Square numbers', fontsize=25)
 plt.xlabel('Value', fontsize=14)
