20b9b8c64b5eec1e5f9580d2951238f27eb991e1
StarZoOm <kulevkir@gmail.com>
2018-10-29 21:46:02 +0300

    first commit

--- scatter_squares.py
+++ scatter_squares.py
@@ -1,12 +1,12 @@
 import matplotlib.pyplot as plt
 
-x_values = list(range(1, 1001))
-y_values = [x**2 for x in x_values]
+x_values = list(range(1, 5001))
+y_values = [x**3 for x in x_values]
 
-plt.scatter(x_values, y_values, edgecolor='none', s=20)
+plt.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Blues, edgecolor='none', s=20)
 plt.title("Square Numbers", fontsize=24)
 plt.xlabel("Value", fontsize=14)
 plt.ylabel("Square of Value", fontsize=14)
 plt.tick_params(axis='both', which='major', labelsize=14)
-plt.axis([0, 1100, 0, 1100000])
+#plt.axis([0, 1100, 0, 1100000])
 plt.show()
